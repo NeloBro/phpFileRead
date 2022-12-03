5b9ca1bf8cd5d7a2d4ef10105adefde7f5d8cdda
NeloBro <95362781+NeloBro@users.noreply.github.com>
2022-12-03 13:24:28 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>FILE</title>
+</head>
+<body>
+	<form method="post" action="action.php">
+		<input type="submit" name="btn" value="OK">
+	</form>
+</body>
+</html>
